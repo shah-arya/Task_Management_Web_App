933e89ab7df3a03298ea65243ab1db7fa14fead6
Arya Shah <arya4shah9@gmail.com>
2025-05-13 09:45:31 -0400

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
+const serverless = require('serverless-http');
 
 const app = express();
 app.use(cors());
@@ -119,7 +120,8 @@ app.delete('/api/tasks/:id', authenticateToken, (req, res) => {
 });
 
 // Starts the server
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+module.exports = serverless(app);
